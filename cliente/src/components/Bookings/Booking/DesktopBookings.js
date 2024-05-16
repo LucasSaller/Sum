7c12f3f82de6d71d6919093436fbfa65941eb164
LucasSaller <sallerlucas@gmail.com>
2024-05-16 03:01:15 -0300

    Update DesktopBookings.js

--- cliente/src/components/Bookings/Booking/DesktopBookings.js
+++ cliente/src/components/Bookings/Booking/DesktopBookings.js
@@ -35,7 +35,7 @@ const DesktopBookings = ({ setCurrentId, bookings, past }) => {
           {bookings.map((booking, id) => (
             <TableRow key={id}>
               <TableCell component="th" scope="row">
-                {booking.name.split(" ")[0]}
+                {booking.name}
               </TableCell>
               <TableCell align="center">{booking.apartment}</TableCell>
               <TableCell align="center">{booking.date}</TableCell>
